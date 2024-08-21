f769327aea734440b61b48e31c61ce00eda94a0e
Felipe da Silva <felipedasilvabispodossantos@gmail.com>
2024-08-20 23:18:15 -0300

    :sparkles: feat: Adicionando funcionalidades de visualização e exclusão de clientes

---
+++ commands/ver_clientes.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../config/Connection.php';
+require_once __DIR__ . '/../config/Segredos.php';
+require_once __DIR__ . '/../src/Cliente.php';
+
+use Mercado\Connection;
+use Mercado\Cliente;
+
+// Conectar ao banco de dados
+$database = new Connection();
+$db = $database->connect();//armazena o método específico de conexão em $db
+
+// Aqui é criada a instância de Cliente, chamando o construtor da classe
+$cliente = new Cliente($db);
+$cliente->verClientes();
--- src/Cliente.php
+++ src/Cliente.php
@@ -26,4 +26,35 @@ class Cliente{
             echo "Erro ao cadastrar cliente.";
         }
     }
+
+    public function verClientes(){
+        echo "Mostrando Clientes...";
+        $sql = "SELECT * FROM ". $this->table;
+        $stmt = $this->conn->query($sql);
+
+        //usa-se contrabarra quando uma classe pertence à um namespace global, ou quando a classe n pertence ao namespace do arquivo
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            echo "ID: " . $row['id'] . "\n";
+            echo "Nome: " . $row['nome'] . "\n";
+            echo "Email: " . $row['email'] . "\n";
+            echo "Telefone: " . $row['telefone'] . "\n";
+            echo "-----------------------\n";
+        }
+    }
+
+    public function excluirCliente($id){
+        echo "Deletando Cliente...";
+        $sql = "DELETE FROM ". $this->table . " WHERE id = :id";
+
+        //usando Prepared Statement para proteger contra sql injection
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
+
+        //Se não houver uma entrada inesperada para escolher o id do cliente a ser deletado...
+        if ($stmt->execute()) {
+            echo "Cliente deletado com sucesso.\n";
+        } else {
+            echo "Erro ao deletar cliente.\n";
+        }
+    }
 }
